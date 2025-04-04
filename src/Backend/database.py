1637384a4152e0594593b849e733e55ed26de38d
idancoh30 <102730179+idancoh30@users.noreply.github.com>
2025-04-04 13:47:34 +0300

    idan

--- src/Backend/database.py
+++ src/Backend/database.py
@@ -8,7 +8,7 @@ from config import DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE
 
 # 1. Build the raw ODBC connection string
 raw_conn_str = (
-    f"Driver={{ODBC Driver 18 for SQL Server}};"
+    f"Driver={{ODBC Driver 17 for SQL Server}};"
     f"Server={DB_SERVER};"
     f"Database={DB_DATABASE};"
     f"Uid={DB_USER};"
--- src/Backend/main.py
+++ src/Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import uvicorn
 # Import route files (we'll create these later)
 from routes import audio_upload
 from routes import auth
@@ -19,3 +19,6 @@ app.add_middleware(
 # Register your API routers
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(audio_upload.router, prefix="/audio", tags=["Audio Upload"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
